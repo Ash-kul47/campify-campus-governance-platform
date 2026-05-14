da3b59a6fdf603998c8da7ca5829a20dc8aa356a
Ashutosh <kulkarniashu306@gmail.com>
2026-05-14 11:48:42 +0530

    Updated Campify project

--- server.js
+++ server.js
@@ -259,7 +259,8 @@ app.get("/whoami", (req, res) => {
 });
 
 
-const port=3000;
-app.listen(port,function(){
-    console.log("server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
